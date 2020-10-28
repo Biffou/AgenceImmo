26ef0a916ddbeb19bea290ebcf3b247003ace32b
benoit <lecerfbenoit@gmail.com>
2020-10-28 17:53:50 +0100

    add first Entity + connect database

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,12 +1,36 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    /**
+     * PropertyController constructor.
+     * @param PropertyRepository $repository
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
 
     /**
      * @Route("/biens", name="property.index")
